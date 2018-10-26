539e8353becc2524b2cea8e524827c71e1693ef2
Lucas Vieira <lucasvieira94@poli.ufrj.br>
2018-10-26 20:40:56 -0300

    Implemented method to save reservation data to our database and its first parameters

--- utils/saveReservationToDB.js
+++ utils/saveReservationToDB.js
@@ -1,3 +1,30 @@
+const uuid = require("uuid");
+const AWS = require("aws-sdk");
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 module.exports = () => {
-  //
+  const date = JSON.stringify(new Date());
+  const params = {
+    TableName: process.env.DYNAMODB_TABLE,
+    Item: {
+      id: uuid.v1(),
+      name:null,
+      email:null,
+      phone:null,
+      reservedAt: date
+      //add rest of the parameters from a reservation here
+    }
+  };
+
+  dynamoDb.put(params, error => {
+    if (error) {
+      console.error(`Error saving data to DynamoDB: ${JSON.stringify(error)}`);
+      return Promise.reject(
+        `Error saving data to DynamoDB: ${JSON.stringify(error)}`
+      );
+    } else {
+      return Promise.resolve(params.Item);
+    }
+  });
 };
